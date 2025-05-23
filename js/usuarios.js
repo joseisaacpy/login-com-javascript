8ad14d85103b701cb062c646b0a47f96d42617a9
José Isaac <joseisaacnascimento@gmail.com>
2025-05-23 18:32:25 -0300

    fix: Retira lista de usuários do gitignore

---
+++ js/usuarios.js
@@ -0,0 +1,14 @@
+let usuarios = [
+  {
+    id: 1,
+    login: "joseisaac",
+    senha: "091010",
+  },
+  {
+    id: 2,
+    login: "mfontenele",
+    senha: "mfontenelebcard",
+  },
+];
+
+export default usuarios; // exporta o arquivo
